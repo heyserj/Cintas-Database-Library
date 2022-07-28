bf7fbd15812c4f60344475f9c4e8a04f91e1a12d
Heyser <HeyserJ@cintas.com>
2022-07-28 13:13:17 -0400

    Adding insert() methods for SQLite login project

--- DatabaseLibrary/src/main/java/com/example/databaselibrary/DBmain.java
+++ DatabaseLibrary/src/main/java/com/example/databaselibrary/DBmain.java
@@ -10,7 +10,9 @@ import androidx.annotation.Nullable;
 
 public class DBmain extends SQLiteOpenHelper {
     private static final String DBNAME = "loggingDB";
-    private static final String TABLE = "loggingTable2";
+    private static final String TABLE1 = "loggingTable2";
+    private static final String TABLE2 = "userDetails";
+    private static final String TABLE3 = "appSessionData";
     private static final int VER = 1;
 
     public DBmain(@Nullable Context context) {
@@ -19,16 +21,21 @@ public class DBmain extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "create table " + TABLE + "(id integer primary key, EventTime text, " +
+        String query1 = "create table " + TABLE1 + "(id integer primary key, EventTime text, " +
                 "SerialNumber text, AppId text default 'CPRNT', EmpID text, Location integer(4), " +
                 "Route integer(2), Day integer(1), Logger text, EventNumber integer(5), " +
                 "EventAdditionalDesc text, EventAdditionalNum integer)";
-        db.execSQL(query);
+        db.execSQL(query1);
+        String query2 = "create table " + TABLE2 + "(id integer primary key, Name text, Email text," +
+                " Password text, DateOfBirth text, Gender text)";
+        db.execSQL(query2);
+        String query3 = "create table " + TABLE3 + "(id integer primary key, EventTime text, EventDesc text)";
+        db.execSQL(query3);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String query = "drop table if exists " + TABLE + "";
+        String query = "drop table if exists " + TABLE1 + "";
         db.execSQL(query);
         onCreate(db);
     }
@@ -41,7 +48,7 @@ public class DBmain extends SQLiteOpenHelper {
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from " + TABLE, null);
+        Cursor res = db.rawQuery("select * from " + TABLE1, null);
         return res;
 
     }
@@ -50,12 +57,6 @@ public class DBmain extends SQLiteOpenHelper {
                           int route, int day, String logger, int eventNum, String eventAdditionalDesc,
                           int eventAdditionalNum) {
 
-        //boolean flag1 = checkEventTimeFormatting(eventTime);
-        //boolean flag5 = checkEventNumberFormatting(eventNum);
-
-        //if (flag2 && flag3 && flag4 && flag5) {
-
-        //At this point, we know the record is valid, and we can insert.
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("EventTime", eventTime);
@@ -70,42 +71,18 @@ public class DBmain extends SQLiteOpenHelper {
         contentValues.put("EventAdditionalDesc", eventAdditionalDesc);
         contentValues.put("EventAdditionalNum", eventAdditionalNum);
 
-        long temp = db.insert(TABLE, null, contentValues);
+        long temp = db.insert(TABLE1, null, contentValues);
         if (temp != -1){
             return true;
         }
         else{
             return false;
         }
-        //return true;
-    }
-
-    private boolean checkEventNumberFormatting(int eventNum) {
-        if (eventNum >= 0 && eventNum <= 21) {
-            return true;
-        }
-        if (eventNum >= 23 && eventNum <= 27) {
-            return true;
-        }
-        switch (eventNum) {
-            case 30:
-                return true;
-            case 32:
-                return true;
-            case 33:
-                return true;
-            case 88:
-                return true;
-            case 99999:
-                return true;
-            default:
-                return false;
-        }
     }
 
     public boolean removeRecord(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        long result = db.delete(TABLE, "id=" + id, null);
+        long result = db.delete(TABLE1, "id=" + id, null);
         if (result != -1) {
             return true;
         } else {
@@ -115,16 +92,13 @@ public class DBmain extends SQLiteOpenHelper {
 
     public void removeAll(){
         SQLiteDatabase db = this.getWritableDatabase();
-        int result = db.delete(TABLE, null, null);
+        int result = db.delete(TABLE1, null, null);
     }
 
     public boolean updateRecord(int id, String eventTime, String serialNum, String empId, int location,
                                 int route, int day, String logger, int eventNum, String eventAdditionalDesc,
                                 int eventAdditionalNum) {
 
-        //boolean flag5 = checkEventNumberFormatting(eventNum);
-
-        //if (flag2 && flag3 && flag4 && flag5) {
         ContentValues contentValues = new ContentValues();
         contentValues.put("EventTime", eventTime);
         contentValues.put("SerialNumber", serialNum);
@@ -139,10 +113,46 @@ public class DBmain extends SQLiteOpenHelper {
         contentValues.put("EventAdditionalNum", eventAdditionalNum);
 
         SQLiteDatabase db = this.getWritableDatabase();
-        long result = db.update(TABLE, contentValues, "id=" + id, null);
+        long result = db.update(TABLE1, contentValues, "id=" + id, null);
         if (result != -1) {
             return true;
         } else {
+            return false;
+        }
+    }
+
+    public boolean insertUserDetails(String name, String email, String password, String dateOfBirth,
+        String gender){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Name", name);
+        contentValues.put("Email", email);
+        contentValues.put("Password", password);
+        contentValues.put("DateOfBirth", dateOfBirth);
+        contentValues.put("Gender", gender);
+
+        long temp = db.insert(TABLE2, null, contentValues);
+        if (temp != -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean insertAppDetails(String eventTime, String eventDescription){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("EventTime", eventTime);
+        contentValues.put("EventDesc", eventDescription);
+
+        long temp = db.insert(TABLE3, null, contentValues);
+        if (temp != -1){
+            return true;
+        }
+        else{
             return false;
         }
     }
